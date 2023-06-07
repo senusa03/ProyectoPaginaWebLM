73584e2660b95778990ecebae04d01ded34b65a5
senusa03 <sergions1609@gmail.com>
2023-06-07 18:03:42 +0200

    Tienda

---
+++ js/tienda.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper-container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    resizeObserver: true,
+  });
