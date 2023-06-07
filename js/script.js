e7eeea05d1bf3d55f87ab3f523794391dd22aa62
senusa03 <sergions1609@gmail.com>
2023-06-07 21:03:13 +0200

    Inicio terminado

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 window.addEventListener('scroll', function () {
-    var izqElement = document.querySelector('.izq');
-    var derElement = document.querySelector('.der');
-    var izqPosition = izqElement.getBoundingClientRect().top;
-    var derPosition = derElement.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.3;
-  
-    if (izqPosition < screenPosition) {
-      izqElement.classList.add('show');
-    }
-  
-    if (derPosition < screenPosition) {
-      derElement.classList.add('show');
-    }
-  });
+  var izqElement = document.querySelector('.izq');
+  var derElement = document.querySelector('.der');
+  var izqPosition = izqElement.getBoundingClientRect().top;
+  var derPosition = derElement.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.3;
 
-  
+  if (izqPosition < screenPosition) {
+    izqElement.classList.add('show');
+  }
+
+  if (derPosition < screenPosition) {
+    derElement.classList.add('show');
+  }
+});
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
