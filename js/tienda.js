e8da29ae0522fb5d969fbb9af732311d56149b2f
senusa03 <sergions1609@gmail.com>
2023-06-07 22:09:30 +0200

    TERMINADO !!

--- js/tienda.js
+++ js/tienda.js
@@ -1,11 +1,20 @@
 var swiper = new Swiper(".swiper-container", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    resizeObserver: true,
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  resizeObserver: true,
+});
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
